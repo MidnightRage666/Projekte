3ed5153b344a67fe095f575cff55d75d7a85b4a6
MidnightRage666 <jesse.ihlenburg@outlook.com>
2026-09-18 11:09:23 +0200

    commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// get the variables
+const err = document.querySelector(".err");
+const inputTask = document.getElementById("input-task");
+const addTaskBtn = document.getElementById("add-task");
+const inputSearch = document.getElementById("search-input");
+const taskList = document.querySelector(".task-list");
+const clearAllBtn = document.querySelector("clear-all");
+
+// add a task
+
+// edit a task
+
+// delete a task
+
+// search task
